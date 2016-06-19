581ffa9ef7d098c1dbe33a161a2db6b43a580ab6
Adam Gothmann <adamgothmann@Adams-MacBook-Pro.local>
2016-06-19 17:56:00 -0500

    appending wrong info

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,6 @@
-console.log('sourced');
-
 $(document).ready(function(){
   console.log('jquery');
+  // updateDom();
   $('#addItem').on('click', function(){
     var input = $('#listInput').val();
     //generating object
@@ -13,19 +12,51 @@ $(document).ready(function(){
     $.ajax({
       type: 'POST',
       url: '/addItem',
-      data: inputObject
-    }); //end addItem ajax
-    $.ajax({
-      type: 'GET',
-      url: '/getItem',
-      success: function(data){
-        console.log(data);
-        showItem(data);
-
+      data: inputObject,
+      success: function() {
       } //end success
-    }); //end retrieveItem ajax
-  }); //end button click
+    }); //end addItem ajax
+    $('#listInput').val('');
+    updateDom();
+  }); //end addItem click
+
+
+}); //end jQuery function
+var updateDom = function(){
+  $('#outputDiv').empty();
+  $.ajax({
+    type: 'GET',
+    url: '/getItem',
+    success: function(data){
+      showItem(data);
+      console.log('something');
+    } //end success
+  }); //end retrieveItem ajax
+};
+
   function showItem(users){
     console.log(users);
-  }
-}); //end jQuery function
+    for( i = 0; i < users.length; i++) {
+    var output = document.createElement('div');
+    var text = "<p>" + users[i].list_item + " status: " + "</p><button id='statusButton'class='statusButton' data-id='" + users[i].id + "'>incomplete</button><button id='deleteButton' class='delete' data-id='" + users[i].id + "'>delete</button>";
+    output.textContent = text;
+    $('body').append(output);
+    } //end for loop
+  } //end showItem
+
+$(document).ready(function(){
+  $('#outputDiv').on('click', '#deleteButton', function(){
+    console.log('delete button');
+    var deleteById = $(this).attr('data-id');
+    console.log(deleteById);
+    var deleteUser = {
+      'id': deleteById
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/deleteItem',
+      data: deleteUser
+    });
+
+  });
+});
--- server/app.js
+++ server/app.js
@@ -24,24 +24,29 @@ app.post('/addItem', urlencodedParser, function(req, res){
   console.log('ing add item: ' + req.body.input);
   pg.connect(connectionString, function(err, client, done){
     client.query('INSERT INTO users (list_item, status, created) VALUES ($1, $2, $3)', [req.body.input, req.body.status, 'now()']);
+    done();
   });
+  res.end();
 });//end addItem
 
 app.get('/getItem', function(req, res){
   console.log('in getItem');
-  // var results = [];
+  var results = [];
   pg.connect(connectionString, function(err, client, done){
     // selects list items
-    var item = client.query('SELECT list_item FROM users WHERE status=false');
-
-    // item.on('row', function(row){
-    //   results.push(row);
-    // });
-    // item.on('end', function(){
-    //   return res.json(results);
-    // });
-    res.send(item);
-
-
+    var item = client.query('SELECT * FROM users');
+    var rows = 0;
+    item.on('row', function(row){
+      results.push(row);
+    });
+    item.on('end', function(){
+      return res.json(results);
+    });
   });
 });//end getItem
+
+app.post('/deleteItem', function(req, res){
+  pg.connect(connectionString, function( err, client, done){
+    client.query('DELETE from users WHERE id=($1)', [req.body.id]);
+  });
+});
